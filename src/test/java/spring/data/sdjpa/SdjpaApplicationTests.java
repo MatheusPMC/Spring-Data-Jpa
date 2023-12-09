d214ac3ce870913d5cc11d2be7302383010cc30f
MatheusPMC <matheus.pmc@hotmail.com>
2023-12-08 21:03:56 -0300

    Criando um entidade

---
+++ src/test/java/spring/data/sdjpa/SdjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
